913fc16a60259d8bff4d02e35be05d3ab88117f4
toiletbril <s9und@outlook.com>
2023-07-22 19:07:15 +0300

    Open up file selection when no arguments are specified

---
+++ src/gui.c
@@ -0,0 +1,53 @@
+#include "gui.h"
+
+#define NOC_FILE_DIALOG_IMPLEMENTATION
+
+#ifdef _WIN32
+    #define NOC_FILE_DIALOG_WIN32
+#else
+    #define NOC_FILE_DIALOG_GTK
+#endif
+
+#include "noc_file_dialog.h"
+
+#define FLAGS (NOC_FILE_DIALOG_OPEN | NOC_FILE_DIALOG_OVERWRITE_CONFIRMATION)
+
+static bool get_dir(const char *file_path, char *buf, size_t size)
+{
+    for (size_t i = strlen(file_path); i > 0; --i) {
+        for (size_t j = 0; j < strlen(DIR_SEP); ++j) {
+            if (file_path[i] == DIR_SEP[j] && size >= i + 1) {
+                memcpy(buf, file_path, i * sizeof(char));
+                buf[i + 1] = '\0';
+
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+bool use_dialog(void)
+{
+    const char *skin_ini_path = noc_file_dialog_open(FLAGS, "skin.ini\0*.ini", ".", PROGRAM_NAME);
+
+    char dir_path[MAX_PATH];
+    get_dir(skin_ini_path, dir_path, MAX_PATH);
+
+    size_t files_created = 0;
+    size_t files_skipped = 0;
+
+    if (is_skin_folder(dir_path)) {
+        smaller_dir(dir_path, &files_created, &files_skipped);
+
+        printf("%s: Successfully traversed '%s'. Files created: %zu, skipped: %zu.\n",
+               PROGRAM_NAME, dir_path, files_created, files_skipped);
+
+        return true;
+    } else {
+        put_error("Is not a skin folder", dir_path);
+
+        return false;
+    }
+}
---
+++ src/gui.h
@@ -0,0 +1,11 @@
+#ifndef GUI_H
+#define GUI_H
+
+#include <stddef.h>
+
+#include "common.h"
+#include "smaller.h"
+
+bool use_dialog(void);
+
+#endif // GUI_H
