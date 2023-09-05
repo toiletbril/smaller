f396e3baa682288b21fe7a811349026c4ef6ae08
toiletbril <s9und@outlook.com>
2023-09-05 03:27:01 +0300

    Suppress warnings from stb

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set CC=clang
-set FLAGS=-O2 -Os -Wall -Wno-deprecated-declarations -Iexternal
+set FLAGS=-Os -Wall -Wextra -Wno-sign-compare -Wno-unused -Wno-deprecated-declarations -Iexternal
 set LIBS=-lcomdlg32
 set FILES=src/main.c src/cli.c src/smaller.c src/gui.c
 
